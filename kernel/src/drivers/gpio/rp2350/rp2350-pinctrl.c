154b8892845a91a4bf92d31e5ac3f6ffc6064040
Philippe Thierry <phil@reseau-libre.net>
2026-06-05 12:12:00 +0200

    rp2350,bsp: adding gpio driver

--- kernel/src/drivers/gpio/rp2350/rp2350-pinctrl.c
+++ kernel/src/drivers/gpio/rp2350/rp2350-pinctrl.c
@@ -7,6 +7,29 @@
 
 kstatus_t gpio_pinctrl_configure(gpio_pinctrl_desc_t pinctrl_desc)
 {
-    (void)pinctrl_desc;
-    return K_STATUS_OKAY;
+    kstatus_t status;
+
+    status = gpio_set_type(pinctrl_desc.port_id, pinctrl_desc.pin, pinctrl_desc.type);
+    if (unlikely(status != K_STATUS_OKAY)) {
+        return status;
+    }
+
+    status = gpio_set_speed(pinctrl_desc.port_id, pinctrl_desc.pin, pinctrl_desc.speed);
+    if (unlikely(status != K_STATUS_OKAY)) {
+        return status;
+    }
+
+    status = gpio_set_pull_mode(pinctrl_desc.port_id, pinctrl_desc.pin, pinctrl_desc.pull_mode);
+    if (unlikely(status != K_STATUS_OKAY)) {
+        return status;
+    }
+
+    if (pinctrl_desc.mode == GPIOx_MODE_AF) {
+        status = gpio_set_af(pinctrl_desc.port_id, pinctrl_desc.pin, pinctrl_desc.altfunc);
+        if (unlikely(status != K_STATUS_OKAY)) {
+            return status;
+        }
+    }
+
+    return gpio_set_mode(pinctrl_desc.port_id, pinctrl_desc.pin, pinctrl_desc.mode);
 }
